b018d4d9b5b7898abfafe5bee5a151fa058481fa
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-15 14:12:50 +0800

    fix: directly using Weak instead of Option
    also, added tree integrity test

--- src/router/tree.rs
+++ src/router/tree.rs
@@ -13,7 +13,7 @@ struct Node {
   fragment: String,
   handlers: Vec<(u32, Handler)>,
   next: HashMap<String, Rc<RefCell<Node>>>,
-  parent: Option<Weak<RefCell<Node>>>
+  parent: Weak<RefCell<Node>>
 }
 
 impl Node {
@@ -22,7 +22,7 @@ impl Node {
       fragment: fragment.to_string(),
       handlers: Vec::new(),
       next: HashMap::new(),
-      parent: None
+      parent: Weak::new()
     }
   }
 }
@@ -61,7 +61,7 @@ impl Tree {
 
           {
             let mut node = node.as_ref().borrow_mut();
-            node.parent = Some(Rc::downgrade(&parent));
+            node.parent = Rc::downgrade(&parent);
           }
 
           node.clone()
@@ -72,8 +72,6 @@ impl Tree {
 
       break 'node parent
     };
-
-    println!("{:?}", self.root);
     
     last_node.as_ref().borrow_mut().handlers.push((self.order, handler));
     self.order += 1;
@@ -89,6 +87,12 @@ mod tree_test {
   macro_rules! node {
       ($v:expr) => {
           $v.as_ref().borrow()
+      };
+  }
+
+  macro_rules! parent {
+      ($v:expr) => {
+          node!($v).parent.upgrade().unwrap()
       };
   }
 
@@ -122,7 +126,7 @@ mod tree_test {
 
     assert_eq!(tree.order, 0);
     assert_eq!(node.fragment, "root");
-    assert!(node.parent.is_none());
+    assert!(node.parent.upgrade().is_none());
     assert_eq!(node.next.len(), 0);
   }
 
@@ -130,7 +134,6 @@ mod tree_test {
     let mut parent = tree.root.clone();
     for (i, fragment) in path.split("/").into_iter().enumerate() {
       let node = {
-        println!("frag: {}, index: {}", fragment, i);
         if i == 0 && fragment == "" { tree.root.clone() } 
         else {
           parent.as_ref().borrow().next.get(fragment).unwrap().clone()
@@ -139,19 +142,14 @@ mod tree_test {
 
       let ref_node = node!(node);
       if i == 0 && fragment == "" {
-        assert!(ref_node.parent.is_none());
-        assert_eq!(ref_node.next.len(), 1);
+        assert!(ref_node.parent.upgrade().is_none());
+        assert!(ref_node.next.len() >= 1);
         continue
       }
 
-      assert!(ref_node.parent.is_some());
-      let parent_interior = ref_node.parent.clone().unwrap();
-      assert!(Rc::ptr_eq(&parent, &parent_interior.upgrade().unwrap()));
+      assert!(ref_node.parent.upgrade().is_some());
+      assert!(Rc::ptr_eq(&parent, &parent!(node)));
       assert!(ref_node.fragment == fragment);
-
-      if i == path.split("/").count() - 1 {
-        assert!(ref_node.next.len() == 0);
-      }
     
       parent = node.clone();
     }
@@ -174,26 +172,96 @@ mod tree_test {
   }
 
   #[test]
-  fn simple_tree_insertion_slashed() {
+  fn slashed_prefix_test() {
     let mut tree = Tree::new();
     let path = "/a/b/c/d";
 
     tree.register(handl!(200, &path));
     assert_eq!(tree.order, 1);
+
+    let node = node!(tree.root);
+    assert!(node.next.len() == 1);
     
     let node = test_node_exist(&tree, path);
-    test_handler_exist(node, 0, 0, 200);
+    test_handler_exist(node.clone(), 0, 0, 200);
+    
+    let node = node!(node);
+    assert!(node.next.len() == 0);
   }
 
   #[test]
-  fn simple_tree_insertion_unslashed() {
+  fn unslashed_prefix_test() {
     let mut tree = Tree::new();
     let path = "a/b/c/d";
 
     tree.register(handl!(200, &path));
     assert_eq!(tree.order, 1);
     
+    let node = node!(tree.root);
+    assert!(node.next.len() == 1);
+
     let node = test_node_exist(&tree, path);
-    test_handler_exist(node, 0, 0, 200);
+    test_handler_exist(node.clone(), 0, 0, 200);
+
+    let node = node!(node);
+    assert!(node.next.len() == 0);
+  }
+
+  #[test]
+  fn deep_tree_integrity_test() {
+    let mut tree = Tree::new();
+    let path_abcd = "a/b/c/d";
+    let path_abxy = "/a/b/x/y";
+    let path_ab = "a/b";
+    let path_xyz = "x/y/z";
+
+    tree.register(handl!(200, &path_abcd));
+    tree.register(handl!(300, &path_abxy));
+    tree.register(handl!(2, &path_ab));
+    tree.register(handl!(200, &path_abxy));
+    tree.register(handl!(300, &path_abcd));
+    tree.register(handl!(999, &path_abxy));
+    tree.register(handl!(199, &path_abcd));
+    tree.register(handl!(1, &path_xyz));
+    tree.register(handl!(2, &path_xyz));
+
+    let ref_node_root = node!(tree.root);
+    assert!(ref_node_root.next.len() == 2);
+
+    let node_ab = test_node_exist(&tree, path_ab);
+    test_handler_exist(node_ab.clone(), 0, 2, 2);
+
+    let ref_node_ab = node!(node_ab);
+    assert!(ref_node_ab.next.len() == 2);
+    assert!(ref_node_ab.handlers.len() == 1);
+
+    let node_abcd = test_node_exist(&tree, path_abcd);
+    test_handler_exist(node_abcd.clone(), 0, 0, 200);
+    test_handler_exist(node_abcd.clone(), 1, 4, 300);
+    test_handler_exist(node_abcd.clone(), 2, 6, 199);
+
+    let ref_node_abcd = node!(node_abcd);
+    assert!(ref_node_abcd.next.len() == 0);
+    assert!(ref_node_abcd.handlers.len() == 3);
+    assert!(Rc::ptr_eq(&node_ab, &parent!(parent!(node_abcd))));
+
+    let node_abxy = test_node_exist(&tree, path_abxy);
+    test_handler_exist(node_abxy.clone(), 0, 1, 300);
+    test_handler_exist(node_abxy.clone(), 1, 3, 200);
+    test_handler_exist(node_abxy.clone(), 2, 5, 999);
+
+    let ref_node_abxy = node!(node_abxy);
+    assert!(ref_node_abxy.next.len() == 0);
+    assert!(ref_node_abxy.handlers.len() == 3);
+    assert!(Rc::ptr_eq(&node_ab, &node!(ref_node_abxy.parent.upgrade().unwrap()).parent.upgrade().unwrap()));
+
+    let node_xyz = test_node_exist(&tree, path_xyz);
+    test_handler_exist(node_xyz.clone(), 0, 7, 1);
+    test_handler_exist(node_xyz.clone(), 1, 8, 2);
+    
+    let ref_node_xyz = node!(node_xyz);
+    assert!(ref_node_xyz.next.len() == 0);
+    assert!(ref_node_xyz.handlers.len() == 2);
+    assert!(Rc::ptr_eq(&tree.root, &parent!(parent!(parent!(node_xyz)))));
   }
 }
